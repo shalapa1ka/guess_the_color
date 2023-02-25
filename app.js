62e917112c5c23ff9c1a66a4b315f10e6dc30ced
shalapa1ka <flecks07@gmail.com>
2023-02-25 14:55:29 +0200

    80% done

---
+++ app.js
@@ -0,0 +1,86 @@
+const easyBtn = document.querySelector('.easy');
+const hardBtn = document.querySelector('.hard');
+const newGameBtn = document.querySelector('.new-game');
+
+const cardList = document.querySelector('main');
+for (let card of cardList.children) {
+    card.addEventListener('click', () => {
+        guess(card.style.backgroundColor);
+    });
+}
+
+const colors = [];
+let guessColor = '';
+
+
+const toggleSelectedBtn = (btn) => {
+    easyBtn.classList.toggle('selected');
+    hardBtn.classList.toggle('selected');
+}
+
+const randColor = () => {
+    return Math.floor(Math.random()*16777215).toString(16);
+}
+
+const shuffleArray = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
+const hexToRgb = (hex) => {
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+    return `${r}, ${g}, ${b}`;
+}
+
+function rgbToHex(col)
+{
+    if(col.charAt(0)==='r')
+    {
+        col=col.replace('rgb(','').replace(')','').split(',');
+        let r=parseInt(col[0], 10).toString(16);
+        let g=parseInt(col[1], 10).toString(16);
+        let b=parseInt(col[2], 10).toString(16);
+        r=r.length===1?'0'+r:r; g=g.length===1?'0'+g:g; b=b.length===1?'0'+b:b;
+        return r + g + b;
+    }
+}
+const updateUI = () => {
+    for (let i = 0; i < 6; i++) {
+        const color = document.querySelector(`.color-${i+1}`);
+        color.style.backgroundColor = `#${colors[i]}`;
+    }
+    document.querySelector('.guess-color').textContent = `RGB (${hexToRgb(guessColor)})`;
+}
+
+Array.prototype.clear = function() {
+    this.length = 0;
+}
+
+const newGame = () => {
+    guessColor = randColor();
+    colors.clear();
+    colors.push(guessColor);
+    for (let i = 0; i < 5; i++) {
+        colors.push(randColor());
+    }
+    shuffleArray(colors);
+    updateUI();
+}
+
+const guess = (color) => {
+    if (rgbToHex(color) === guessColor) {
+        alert('You win!');
+    } else {
+        alert('You lose!');
+    }
+}
+
+easyBtn.addEventListener('click', toggleSelectedBtn);
+hardBtn.addEventListener('click', toggleSelectedBtn);
+
+newGameBtn.addEventListener('click', newGame);
